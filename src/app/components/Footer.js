f60120577eaa508975a186c4c1b5d3098c4eb50a
Your Name <you@example.com>
2026-03-31 23:50:06 -0400

    changes

---
+++ src/app/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>&copy; 2026 Roblox Group Document Manager. All rights reserved.</p>
+            <p>Contact us:
+                <a href="mailto:camolid93@gmail.com">camolid93@gmail.com</a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
