7d3e7679a60e883f2cfec70270f8d7ecca7eedc3
Your Name <you@example.com>
2026-04-01 22:25:47 -0400

    adding topbar

---
+++ src/app/components/Topbar.js
@@ -0,0 +1,16 @@
+function Topbar() {
+    return (
+<div className="Topbar">
+    <nav>
+      <ul>
+        <li><a href="#welcome">Home</a></li>
+        <li><a href="#features">Features</a></li>
+        <li><a href="#getting-started">Getting Started</a></li>
+        <li><a href="#contact">Contact Us</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+      </ul>
+    </nav>
+  </div>
+    );
+}
+export default Topbar;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
+import Topbar from "./components/Topbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
+        <Topbar />
         {children}
         <Footer />
       </body>
