3e68dd2529996211dbf884aa8ec1c5b67e5775a3
Your Name <you@example.com>
2026-04-01 22:29:23 -0400

    attempting to bug fix

--- src/app/components/Topbar.js
+++ src/app/components/Topbar.js
@@ -13,4 +13,5 @@ function Topbar() {
   </div>
     );
 }
+
 export default Topbar;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
-import Topbar from "./components/Topbar";
+import Topbar from "./components/Topbar.js";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
