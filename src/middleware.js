7fb25a945536d97c8b4967fb96308d65ae051b9e
Your Name <you@example.com>
2026-04-10 22:32:57 -0400

    fix IS GOING TO WORK BEFORE I GO CRAZYYYYY

--- src/middleware.js
+++ src/middleware.js
@@ -5,7 +5,7 @@ const isProtectedRoute = createRouteMatcher([
   "/workspace(.*)",
 ]);
 
-export const proxy = clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
 });
 
